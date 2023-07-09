481b113b7cf71efacf73ec7041ce3c1d4d40779b
boboyevmuhammadi98 <boboyevmuhammadi98@gmail.com>
2023-07-09 17:15:31 +0500

    changefour

---
+++ num/src/main/java/com/example/num/NumApplication.java
@@ -0,0 +1,13 @@
+package com.example.num;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumApplication.class, args);
+	}
+
+}
--- num/src/test/java/com/example/num/NumApplicationTests.java
+++ num/src/test/java/com/example/num/NumApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.num;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo1ApplicationTests {
+class NumApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
