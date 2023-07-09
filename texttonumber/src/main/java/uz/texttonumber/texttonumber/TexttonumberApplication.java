2ef05457ebfe8ea13297278bc2f5164c63e5738a
boboyevmuhammadi98 <boboyevmuhammadi98@gmail.com>
2023-07-09 14:34:12 +0500

    changeone

---
+++ texttonumber/src/main/java/uz/texttonumber/texttonumber/TexttonumberApplication.java
@@ -0,0 +1,13 @@
+package uz.texttonumber.texttonumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexttonumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TexttonumberApplication.class, args);
+    }
+
+}
---
+++ texttonumber/src/test/java/uz/texttonumber/texttonumber/TexttonumberApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.texttonumber.texttonumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexttonumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
