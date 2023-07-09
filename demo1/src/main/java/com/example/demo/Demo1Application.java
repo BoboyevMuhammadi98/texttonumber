0fc193d452aac1668d51eb3a5d192763882cc53a
boboyevmuhammadi98 <boboyevmuhammadi98@gmail.com>
2023-07-09 15:14:59 +0500

    changethree

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,13 @@
-package uz.texttonumber.texttonumber;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TexttonumberApplication {
+public class Demo1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TexttonumberApplication.class, args);
+        SpringApplication.run(Demo1Application.class, args);
     }
 
 }
--- demo1/src/main/java/com/example/demo/GetController.java
+++ demo1/src/main/java/com/example/demo/GetController.java
@@ -1,6 +1,9 @@
-package uz.texttonumber.texttonumber;
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/get")
